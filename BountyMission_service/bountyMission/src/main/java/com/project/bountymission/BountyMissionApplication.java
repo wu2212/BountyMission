582e12c2c65a21ac5c7a59b9873cc4888328b213
wu <1085406285@qq.com>
2025-02-28 22:50:13 +0800

    前后端项目初始化

---
+++ BountyMission_service/bountyMission/src/main/java/com/project/bountymission/BountyMissionApplication.java
@@ -0,0 +1,13 @@
+package com.project.bountymission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BountyMissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BountyMissionApplication.class, args);
+    }
+
+}
---
+++ BountyMission_service/bountyMission/src/test/java/com/project/bountymission/BountyMissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bountymission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BountyMissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
